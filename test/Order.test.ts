5903fc500b599025155b94477bcf8d0dd88d5f50
phoenix <ahmmzxf@163.com>
2023-11-23 12:06:53 +0800

    test for order amount calc

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,4 +1,6 @@
-import { MarketsInfoData, useMarketsInfo } from "../src/gmx/domain/synthetics/markets";
+import { BigNumber } from "ethers";
+import { useMarketsInfo } from "../src/gmx/domain/synthetics/markets";
+import { getPositionKey, usePositionsInfo } from "../src/gmx/domain/synthetics/positions";
 import { getIncreasePositionAmounts } from "../src/gmx/domain/synthetics/trade";
 
 
@@ -8,10 +10,39 @@ describe("orders", () => {
     const account = '0x23b27875ad09d21517101a7f83499c38f7ec2d2a';
     const marketAddress = "0x47c031236e19d024b42f8AE6780E44A573170703";
     const collateralTokenAddress = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+    const indexTokenAddress = "0x47904963fc8b2340414262125aF798B9655E58Cd";
+    const isLong = true;
     it("increase amounts", async () => {
         const { marketsInfoData, tokensData } = await useMarketsInfo(chainId, account);
         let market = marketsInfoData![marketAddress]
         let collateralToken = tokensData![collateralTokenAddress]
-    });
+
+        const { positionsInfoData } = await usePositionsInfo(chainId, {
+            account: account,
+            marketsInfoData: marketsInfoData,
+            tokensData: tokensData,
+            showPnlInLeverage: false
+        })
+
+        const posKey = getPositionKey(account, marketAddress, collateralTokenAddress, isLong)
+
+        debugger
+        const increasePositionAmounts = getIncreasePositionAmounts({
+            marketInfo: market,
+            indexToken: tokensData![indexTokenAddress],
+            initialCollateralToken: collateralToken,
+            collateralToken: collateralToken,
+            isLong: isLong,
+            initialCollateralAmount: BigNumber.from(64),
+            position: positionsInfoData![posKey],
+            leverage: BigNumber.from(10),
+            indexTokenAmount: BigNumber.from(100),
+            userReferralInfo: undefined,
+            strategy: "leverageByCollateral",
+            findSwapPath: () => undefined,
+        })
+
+        expect(increasePositionAmounts).not.toBeNull()
+    }, 1e6);
 
 });
--- test/Positions.test.ts
+++ test/Positions.test.ts
@@ -4,18 +4,9 @@ import {usePositionsInfo } from "../src/gmx/domain/synthetics/positions"
 import { useMarketsInfo } from "../src/gmx/domain/synthetics/markets"
 
 describe("positions", () => {
-    // it("use positions test", async () => {
-    //     expect.assertions(1);
-    //     const res = await usePositions(42161, { account: "0x23b27875ad09d21517101a7f83499c38f7ec2d2a" });
-    //     console.log("positions", res)
-    //     expect(res).not.toBeNull()
-    // }, 120000);
-
     it("positions", async () => {
         const chainId = 42161;
         const account = "0x23b27875ad09d21517101a7f83499c38f7ec2d2a";
-        // const account = "0x23B27875ad09d21517101a7f83499C38F7eC2D2a";
-        debugger
         const markets = await useMarketsInfo(chainId, account);
         const positions = await usePositionsInfo(chainId, {
             account: account,
@@ -23,7 +14,6 @@ describe("positions", () => {
             tokensData: markets.tokensData,
             showPnlInLeverage: false
         })
-
-        console.log("positions info", positions)
+        expect(positions).not.toBeNull()
     }, 1e6);
 })
