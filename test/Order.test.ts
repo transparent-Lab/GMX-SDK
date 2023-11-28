b2c944152eba98f30278424964a4bda21cdb2401
phoenix <ahmmzxf@163.com>
2023-11-28 16:57:15 +0800

    order methods

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,9 +1,9 @@
 import { BigNumber, Contract, Wallet } from "ethers";
 import { useMarketsInfo } from "../src/gmx/domain/synthetics/markets";
 import { getPositionKey, usePositionsInfo } from "../src/gmx/domain/synthetics/positions";
-import { getIncreasePositionAmounts, useSwapRoutes } from "../src/gmx/domain/synthetics/trade";
-import { estimateExecuteIncreaseOrderGasLimit, gasLimits, getExecutionFee, useGasPrice } from "../src/gmx/domain/synthetics/fees";
-import { createIncreaseOrderTxn } from '../src/gmx/lib/order';
+import { getDecreasePositionAmounts, getIncreasePositionAmounts, useSwapRoutes } from "../src/gmx/domain/synthetics/trade";
+import { estimateExecuteDecreaseOrderGasLimit, estimateExecuteIncreaseOrderGasLimit, gasLimits, getExecutionFee, useGasPrice } from "../src/gmx/domain/synthetics/fees";
+import { createDecreaseOrderTxn, createIncreaseOrderTxn } from '../src/gmx/lib/order';
 import { OrderType } from "../src/gmx/domain/synthetics/orders";
 import { getContract } from "../src/gmx/config/contracts";
 import ExchangeRouter from "../src/gmx/abis/ExchangeRouter.json";
@@ -15,23 +15,19 @@ import Token from "../src/gmx/abis/Token.json";
 describe("orders", () => {
 
     const chainId = 42161;
-    // const account = '0x23b27875ad09d21517101a7f83499c38f7ec2d2a';
     const account = "0x23B27875ad09d21517101a7f83499C38F7eC2D2a";
     const marketAddress = "0x47c031236e19d024b42f8AE6780E44A573170703";
     const collateralTokenAddress = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
-    // const indexTokenAddress = "0x47904963fc8b2340414262125aF798B9655E58Cd";
-    const isLong = true;
+    const isLong = false;
     const leverage = BigNumber.from(20_000); // x2
     const initialCollateralAmount = BigNumber.from(10_000_000)
     const priKey = process.env.PRIVATE_KEY
 
-    
-    it("increase", async () => {
+    async function fetchInfos() {
         const { marketsInfoData, tokensData } = await useMarketsInfo(chainId, account);
         let market = marketsInfoData![marketAddress]
         let collateralToken = tokensData![collateralTokenAddress]
 
-        
         const { positionsInfoData } = await usePositionsInfo(chainId, {
             account: account,
             marketsInfoData: marketsInfoData,
@@ -40,12 +36,24 @@ describe("orders", () => {
         })
 
         const posKey = getPositionKey(account, marketAddress, collateralTokenAddress, isLong)
+        return {
+            position: positionsInfoData![posKey],
+            marketsInfoData,
+            market,
+            collateralToken,
+            tokensData,
+        }
+    }
+
+
+    it("increase", async () => {
+        const { position, market, collateralToken, tokensData, marketsInfoData } = await fetchInfos();
 
         const swapRoute = useSwapRoutes(chainId, {
             marketsInfoData,
             fromTokenAddress: collateralTokenAddress,
             toTokenAddress: collateralTokenAddress,
-          });
+        });
 
         debugger
         const increaseAmounts = getIncreasePositionAmounts({
@@ -55,9 +63,9 @@ describe("orders", () => {
             collateralToken: collateralToken,
             isLong: isLong,
             initialCollateralAmount: initialCollateralAmount,
-            position: positionsInfoData![posKey],
+            position: position,
             leverage: leverage,
-            indexTokenAmount: BigNumber.from(100000),
+            indexTokenAmount: BigNumber.from(0),
             userReferralInfo: undefined,
             strategy: "leverageByCollateral",
             findSwapPath: swapRoute.findSwapPath,
@@ -95,8 +103,6 @@ describe("orders", () => {
 
         expect(tx).not.toBeNull()
 
-
-
         // sign the tx
         const provider = new Wallet(priKey!, getProvider());
 
@@ -111,6 +117,65 @@ describe("orders", () => {
             console.log(approveRes)
         }
 
+        const exchangeRouter = new Contract(getContract(chainId, "ExchangeRouter"), ExchangeRouter.abi, provider);
+        const res = await callContract(chainId, exchangeRouter, "multicall", [tx], {
+            value: executionFee?.feeTokenAmount!
+        });
+        expect(res).not.toBeNull()
+    }, 1e6);
+
+    it("decrease", async () => {
+        debugger
+        const { position, market, collateralToken, tokensData } = await fetchInfos();
+
+        const decreaseAmounts = getDecreasePositionAmounts({
+            marketInfo: market,
+            collateralToken: collateralToken,
+            isLong: isLong,
+            position: position,
+            userReferralInfo: undefined,
+            closeSizeUsd: BigNumber.from(5),
+            keepLeverage: false,
+            minCollateralUsd: BigNumber.from(0),
+            minPositionSizeUsd: BigNumber.from(0)
+        })
+
+        expect(decreaseAmounts).not.toBeNull()
+
+        const _gasLimits = await gasLimits(42161)
+        expect(_gasLimits).not.toBeNull()
+
+        const estimatedGas = estimateExecuteDecreaseOrderGasLimit(_gasLimits, {
+            swapsCount: 0,
+        });
+
+        const { gasPrice } = await useGasPrice(chainId);
+        const executionFee = getExecutionFee(_gasLimits, tokensData!, estimatedGas, gasPrice!)
+        expect(executionFee).not.toBeNull()
+
+        const tx = await createDecreaseOrderTxn(chainId, {
+            account: account,
+            marketAddress: marketAddress,
+            initialCollateralAddress: collateralTokenAddress,
+            initialCollateralDeltaAmount: initialCollateralAmount,
+            swapPath: [],
+            receiveTokenAddress: collateralTokenAddress,
+            sizeDeltaUsd: decreaseAmounts.sizeDeltaUsd,
+            sizeDeltaInTokens: decreaseAmounts.sizeDeltaInTokens,
+            acceptablePrice: decreaseAmounts.acceptablePrice,
+            triggerPrice: decreaseAmounts.triggerPrice,
+            minOutputUsd: BigNumber.from(0),
+            isLong,
+            decreasePositionSwapType: decreaseAmounts.decreaseSwapType,
+            orderType: OrderType.MarketDecrease,
+            executionFee: executionFee?.feeTokenAmount!,
+            allowedSlippage: 0,
+            referralCode: undefined,
+            indexToken: market.indexToken,
+            tokensData: tokensData!
+        });
+
+        const provider = new Wallet(priKey!, getProvider());
         const exchangeRouter = new Contract(getContract(chainId, "ExchangeRouter"), ExchangeRouter.abi, provider);
         const res = await callContract(chainId, exchangeRouter, "multicall", [tx], {
             value: executionFee?.feeTokenAmount!
--- test/Positions.test.ts
+++ test/Positions.test.ts
@@ -5,6 +5,7 @@ import { useMarketsInfo } from "../src/gmx/domain/synthetics/markets"
 
 describe("positions", () => {
     it("positions", async () => {
+        debugger
         const chainId = 42161;
         const account = "0x23b27875ad09d21517101a7f83499c38f7ec2d2a";
         const markets = await useMarketsInfo(chainId, account);
